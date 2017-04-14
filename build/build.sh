01b9ed0b684ae0411bd65f0f0192fbfba4d8455e
fwbrasil <fwbrasil@gmail.com>
2017-04-13 22:28:25 -0700

    use default ssh config permissions

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
-chown root ~/.ssh/config
-chmod 644 ~/.ssh/config
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]
 then
