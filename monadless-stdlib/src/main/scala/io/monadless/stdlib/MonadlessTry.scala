90907145d0c5986dcd193817f02dd418b86cfda8
fwbrasil <fwbrasil@gmail.com>
2017-04-18 00:45:19 -0700

    remove unnecessary method MonadlessTry.apply

--- monadless-stdlib/src/main/scala/io/monadless/stdlib/MonadlessTry.scala
+++ monadless-stdlib/src/main/scala/io/monadless/stdlib/MonadlessTry.scala
@@ -6,8 +6,6 @@ import scala.util.control.NonFatal
 
 trait MonadlessTry extends Monadless[Try] {
 
-  def apply[T](v: => T) = Try(v)
-
   def collect[T](list: List[Try[T]]): Try[List[T]] =
     list.foldLeft(Try(List.empty[T])) {
       (acc, item) =>
@@ -17,7 +15,8 @@ trait MonadlessTry extends Monadless[Try] {
         } yield l :+ i
     }
 
-  def rescue[T](m: Try[T])(pf: PartialFunction[Throwable, Try[T]]) = m.recoverWith(pf)
+  def rescue[T](m: Try[T])(pf: PartialFunction[Throwable, Try[T]]) =
+    m.recoverWith(pf)
 
   def ensure[T](m: Try[T])(f: => Unit) = {
     try f
